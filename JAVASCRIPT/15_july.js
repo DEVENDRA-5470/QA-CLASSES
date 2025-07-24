ccd925495513631c9cba3de7e92d26f5247ab97a
DEVENDRA-5470 <your_email@example.com>
2025-07-24 12:17:40 +0530

    JAVASCRIPT

---
+++ JAVASCRIPT/15_july.js
@@ -0,0 +1,26 @@
+// let var const 
+
+// var a=10.10;
+// var a="dev";
+// console.log(a)
+// console.log(typeof(a))
+
+// let a=10.10;
+// let a="dev"
+// console.log(a)
+// console.log(typeof(a))
+
+let a;
+a=10;
+a="dev"
+console.log(a)
+console.log(typeof(a))
+
+// const pi="3.14"
+// const pi
+
+
+let x=200;
+let y=300;
+let z=x;
+x=y
