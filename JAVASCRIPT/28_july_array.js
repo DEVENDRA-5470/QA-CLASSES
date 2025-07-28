8731014f69e39802bcddc039ee471c82ca0ac1c9
DEVENDRA-5470 <your_email@example.com>
2025-07-28 15:46:02 +0530

    updated

---
+++ JAVASCRIPT/28_july_array.js
@@ -0,0 +1,105 @@
+// array ,string ,objects ,oops, dom.
+// Array defination
+// Array creation
+// Array accessing
+// Array updation
+// Array traversing
+// Array built-in methods
+// Array assignment
+
+// What is array and its properties.
+// Array is an dynamic data structure in javascript used to store multiple data/value/information of different types of data in a single variable/object.
+
+// Properties
+// 1.Array are mutable in nature.
+// 2.Array are indexed,ordered and can be sliced.
+// 3.Array can be hetrogenous and homogenous.
+// 4.Dyanmic memory management allocation.
+
+// Array creation
+// 1.using new Array().
+// 2.Using [].
+
+// let stu_marks=[45,89,55,89,39,53,68] // homogenous
+// console.log(stu_marks)
+
+// let new_stu_marks=new Array("dev","aman") // homogenous
+// console.log(new_stu_marks)
+
+// let stu_details=[45,90,true,false,"noida sec-15",['metro city']] // hetrogenous
+// console.log(stu_details)
+
+
+// Array accessing
+// let stu_details=[45,90,true,false,"noida sec-15",['metro city',['aman',"shivam",'shubham']],[90,100,40,50,60]]
+// console.log(stu_details.length)
+// console.log(stu_details[5][1][2])
+// console.log(stu_details[5][1][1])
+
+
+// let new_arr=[ [3,8,0,['d','e','g'],'aman',89] ,90]
+// console.log(new_arr[0][3][2])
+
+
+// Array updation
+// let stu_marks=[45,89,55,89,39,53,68]
+// stu_marks[5]=300000;
+// stu_marks[5]="shivam"
+// console.log(stu_marks)
+
+// let new_arr=[ [3,8,0,['d','e','g'],'aman',89] ,90]
+// new_arr[0][4]="javascript"
+// console.log(new_arr)
+
+
+// let new_arr1=[ [3,8,0,['d','e','g'],'aman',89] ,90]
+// new_arr1[4]="how are you"
+// console.log(new_arr1)
+
+
+// Array traversing
+// // for loop
+// let stu_marks=[45,89,55,89,39,53,68]
+// for (let i=0 ; i<stu_marks.length ;i++){
+//     console.log(stu_marks[i])
+// }
+
+
+// // while loop
+// let stu_marks=[45,89,55,89,39,53,68]
+// let i=0;
+// while (i<stu_marks.length){
+//     console.log(stu_marks[i])
+//     i++
+// }
+
+// // do while loop
+// let stu_marks=[45,89,55,89,39,53,68]
+// let i=0;
+// do{
+//     console.log(stu_marks[i])
+//     i++;
+// }while(i<stu_marks.length)
+
+
+// // for of
+// let stu_marks=[45,89,55,89,39,53,68]
+// for (v of stu_marks){
+//     console.log(v)
+// }
+
+// for in 
+// let stu_marks=[45,89,55,89,39,53,68]
+// for (i in stu_marks){
+//     console.log(stu_marks[i])
+// }
+
+
+
+
+
+
+
+// Array traversing
+
+
